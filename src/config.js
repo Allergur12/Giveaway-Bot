4ed9b5a8fb2879928ec46dc65ddea9d4b84c1e01
Alejandro <101129666+Allergur12@users.noreply.github.com>
2022-09-04 11:43:14 +0200

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,35 @@
+module.exports = {
+    token: "BOT_TOKEN",
+    mongodb: "mongodb+srv://",
+    prefix: "!",
+    logs: "", // id del canal para enviar logs de los giveaways
+  
+ auth: {
+		support: "",// link del servidor de mind shop u otro 
+	
+	},
+ embeds: {
+		color: "#7B98EE", 
+		footers: "Giveaway"
+
+  },
+  
+  owner: {
+		id: "", 
+		name: "user#0000" // owner 
+	},
+ 
+ 	basiclang: "english",
+	
+	giveaway: {
+		hostedBy: true, 
+		
+		reaction: "🎉", 
+		everyoneMention: false,
+		
+		grole: "Giveaway Manger", 
+		
+		lastchanceenabled: false,
+	},
+	
+}
